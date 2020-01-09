64a230447ca79686a708b39d27b3ad9c3e9c6b95
James Robbins <jameslrobbins86@gmail.com>
2020-01-09 11:45:26 -0600

    Working on Grocery List

--- client/src/components/Ingredients.js
+++ client/src/components/Ingredients.js
@@ -3,15 +3,7 @@ import React from "react";
 function Ingredients() {
     return (
         <div>
-            <h1>Ingredients</h1>
-            Steak<br />
-            Green Peppers<br />
-            Onions<br />
-            Jalepenos<br />
-            Cheese<br />
-            Milk<br />
-            Shrimp<br />
-            Butter<br />
+           
         </div>
     );
 }
--- client/src/pages/GroceryList.js
+++ client/src/pages/GroceryList.js
@@ -1,10 +1,36 @@
 import React, { Component } from "react";
 import Logo from "../components/Logo";
-import Recipes from "../components/Recipes";
+import RecipeList from "../components/Recipes";
 import Ingredients from "../components/Ingredients";
+import recipes from "../testRecipes.json";
+import Wrapper from "../components/Wrapper";
 
 
 class List extends Component {
+
+    state = {
+        recipes,
+        clickedRecipes: [],
+        groceryList: [],
+        test: 0
+      }
+
+      handleClick = (id, ingredients) => {
+
+        let clickedRecipes = this.state.clickedRecipes;
+        let groceryList = this.state.groceryList
+
+        clickedRecipes.push(id)
+        groceryList.push(ingredients)
+        console.log("Ingredients " + groceryList)
+        console.log(clickedRecipes)
+
+
+
+      }
+
+     
+      
     render() {
         return (
             <div className="container">
@@ -14,14 +40,36 @@ class List extends Component {
                             <div className="row">
                         <div className="col-md-6">
                             <div className="col-md-12 recipes">
-                            <Recipes />
+                            <Wrapper>
+                                <h1>Recipes</h1>
+                                {this.state.recipes.map(recipe => (
+                                    <RecipeList
+                                        id={recipe.id}
+                                        key={recipe.id}
+                                        name={recipe.name}
+                                        ingredients={recipe.ingredients}
+                                        handleClick={this.handleClick}
+                                    
+                                    />
+                                ))}
+                             </Wrapper>
                             </div>
                         </div>
 
                         <div className="col-md-6">
                             <div className="col-md-12 ingredients">
-                            <Ingredients />
-                            </div>
+                                    <h1>Ingredients</h1>
+                                    <Wrapper>
+
+                                        <Ingredients 
+                                        
+                                        />  
+
+                                    
+                                    </Wrapper>
+                                  
+
+                                </div>
                         </div>
                     </div>
                 </div>
